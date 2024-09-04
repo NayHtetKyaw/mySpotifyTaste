96b4a3e970047a9b085fd8d0c2b5d1632f222f56
NayHtetKyaw <nayhtetkyaw.dev@gmail.com>
2024-09-04 22:36:38 +0700

    tailwind

---
+++ resources/views/itworks.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>It works!</title>
+        @vite('resources/css/app.css')
+    </head>
+    <body>
+        <h1 class="text-3xl text-amber-300">It works!</h1>
+        {{-- <x-header title="about it works" subtitle="yes it is working" /> --}}
+    </body>
+</html>
