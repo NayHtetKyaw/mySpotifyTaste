7a04a01474c2de21aeee1558890bf5d8d3fbb00d
Pumin Wangprasit <savee2011@hotmail.com>
2024-09-26 20:50:39 +0700

    Add profile page

---
+++ frontend/app/profile/page.tsx
@@ -0,0 +1,11 @@
+import {Title, Container, Text} from "@mantine/core";
+import PageHeader from "@components/ui/page-header";
+
+export default function HomePage(): JSX.Element {
+    return (
+        <Container fluid>
+
+        </Container>
+    );
+}
+
