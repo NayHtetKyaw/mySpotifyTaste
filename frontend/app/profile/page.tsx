e08919a49a6ee1e688339fc0b504b18c7e17b142
Pumin Wangprasit <savee2011@hotmail.com>
2024-10-02 15:51:56 +0700

    remove all changes and re-work

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,7 +1,6 @@
 
 import { Container } from "@mantine/core";
 import PageHeader from "@components/ui/page-header";
-import Userprofile from "@components/ui/user_profile/user-profile";
 
 export default function ProfilePage(): JSX.Element {
     return (
@@ -12,7 +11,6 @@ export default function ProfilePage(): JSX.Element {
                 songName="Sukida"
                 artistName="YOASOBI"
             />
-            <Userprofile></Userprofile>
-        </Container>
+         </Container>
     );
 }
---
+++ frontend/components/ui/overview.tsx
@@ -0,0 +1,9 @@
+
+import { Text } from "@mantine/core";
+
+export default function Overview(): JSX.Element {
+    return (
+        <Text> TEST</Text>
+
+    );
+}
