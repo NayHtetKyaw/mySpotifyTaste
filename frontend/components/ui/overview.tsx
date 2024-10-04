2889287bb48bf5da35b5a06a863ad444bf3e254c
Pumin Wangprasit <savee2011@hotmail.com>
2024-10-04 22:55:25 +0700

    add static lists into <stack>

--- frontend/components/ui/overview.tsx
+++ frontend/components/ui/overview.tsx
@@ -1,75 +1,83 @@
-
-import { Card, Group, Text, Avatar, Flex } from "@mantine/core";
+import { Card, Text, Flex, Stack } from "@mantine/core";
 import Image from "next/image";
+
+interface Artist {
+    name: string;
+}
+
+interface Song {
+    name: string;
+    artist: string;
+}
+
+const artistsData: Artist[] = [
+    { name: "Aimer" },
+    { name: "DUSTCELL" },
+    { name: "Hiroyuki Sawano" },
+    { name: "Artist 4" },
+    { name: "Artist 5" },
+    { name: "Artist 6" },
+    { name: "Artist 7" },
+    { name: "Artist 8" },
+    { name: "Artist 9" },
+    { name: "Artist 10" },
+];
+
+const songsData: Song[] = [
+    { name: "Brave Shine", artist: "Aimer" },
+    { name: "Crying My Soul", artist: "DUSTCELL" },
+    { name: "Before My Body Is Dry", artist: "Hiroyuki Sawano" },
+    { name: "LAST STARDUST", artist: "Aimer" },
+    { name: "Self-Hypnosis", artist: "DUSTCELL" },
+    { name: "TRNTY", artist: "Hiroyuki Sawano" },
+    { name: "RE:I AM", artist: "Aimer" },
+    { name: "The Lost", artist: "DUSTCELL" },
+    { name: "aLIEz", artist: "Hiroyuki Sawano" },
+    { name: "I Beg You", artist: "Aimer" },
+];
 
 export default function Overview(): JSX.Element {
     return (
-
-        <Flex p="lg" justify="center" wrap="wrap" >
-            <Card
-                shadow="xs"
-                padding="xl"
-                radius="md"
-                m="lg"
-                className="min-w-1/2 w-[30%]">
-                <Text size="xl" fw={700} className="text-center"> TOP ARTIST</Text>
+        <Flex p="lg" justify="center" wrap="wrap">
+            <Card shadow="xs" padding="xl" radius="md" m="lg" className="min-w-80 w-[30%] h-[52rem]">
+                <Text size="xl" fw={800} className="text-center">TOP ARTIST</Text>
 
                 <div className="flex justify-center items-center m-4 h-36">
-                    <Image src="/assets/images/profile.png"
-                        width={110}
-                        height={110}
-                        alt="profile"
-                        className="relative duration-150 -left-5 hover:scale-110 hover:-translate-x-4 hover:-rotate-2" />
-
-                    <Image src="/assets/images/profile.png"
-                        width={110}
-                        height={110}
-                        alt="profile"
-                        className="relative duration-150 -right-5 hover:scale-110 hover:translate-x-4 hover:rotate-2" />
-
-                    <Image src="/assets/images/profile.png"
-                        width={125}
-                        height={125}
-                        alt="profile"
-                        className=" absolute duration-150 hover:scale-110" />
-                </div>
-            </Card>
-
-
-            <Card
-                shadow="xs"
-                padding="xl"
-                radius="md"
-                m="lg"
-                className="min-w-1/2 w-[30%]">
-                <Text size="xl" fw={700} className="text-center"> TOP SONG</Text>
-
-                <div className="flex justify-center items-center m-4 h-36">
-                    <Image src="/assets/images/profile.png"
-                        width={110}
-                        height={110}
-                        alt="profile"
-                        className="relative duration-150 -left-5 hover:scale-110 hover:-translate-x-4 hover:-rotate-2" />
-
-                    <Image src="/assets/images/profile.png"
-                        width={110}
-                        height={110}
-                        alt="profile"
-                        className="relative duration-150 -right-5 hover:scale-110 hover:translate-x-4 hover:rotate-2" />
-
-                    <Image src="/assets/images/profile.png"
-                        width={125}
-                        height={125}
-                        alt="profile"
-                        className=" absolute duration-150 hover:scale-110" />
+                    <Image src="/assets/images/profile.png" width={110} height={110} alt="profile" className="relative duration-150 -left-5 hover:scale-110 hover:-translate-x-4 hover:-rotate-2" />
+                    <Image src="/assets/images/profile.png" width={110} height={110} alt="profile" className="relative duration-150 -right-5 hover:scale-110 hover:translate-x-4 hover:rotate-2" />
+                    <Image src="/assets/images/profile.png" width={125} height={125} alt="profile" className="absolute duration-150 hover:scale-110" />
                 </div>
 
 
-
-
+                <Stack h={200} w="full" align="stretch" justify="flex-start" gap="sm">
+                    {artistsData.map((artist, index) => (
+                        <Text key={index} size="lg" fw={600} mt="sm" className="text-left">
+                            {index + 1}. {artist.name}
+                        </Text>
+                    ))}
+                </Stack>
             </Card>
-        </Flex >
 
 
+            <Card shadow="xs" padding="xl" radius="md" m="lg" className="min-w-80 w-[30%] h-[66rem]">
+                <Text size="xl" fw={800} className="text-center">TOP SONG</Text>
+
+                <div className="flex justify-center items-center m-4 h-36">
+                    <Image src="/assets/images/profile.png" width={110} height={110} alt="profile" className="relative duration-150 -left-5 hover:scale-110 hover:-translate-x-4 hover:-rotate-2" />
+                    <Image src="/assets/images/profile.png" width={110} height={110} alt="profile" className="relative duration-150 -right-5 hover:scale-110 hover:translate-x-4 hover:rotate-2" />
+                    <Image src="/assets/images/profile.png" width={125} height={125} alt="profile" className="absolute duration-150 hover:scale-110" />
+                </div>
+
+
+                <Stack h={200} w="full" align="stretch" justify="flex-start" gap="sm">
+                    {songsData.map((song, index) => (
+                        <Text key={index} size="lg" fw={600} mt="sm" className="text-left flex flex-col">
+                            {index + 1}. {song.name}
+                            <Text size="md">{song.artist}</Text>
+                        </Text>
+                    ))}
+                </Stack>
+            </Card>
+        </Flex>
     );
 }
