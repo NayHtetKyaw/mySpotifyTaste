e4b3c568f98032aa481a5b1e7eb20f98c2e44460
Bammcool2011 <savee2011@hotmail.com>
2024-10-07 22:40:50 +0700

    update

--- frontend/app/components/ui/header-tabs.tsx
+++ frontend/app/components/ui/header-tabs.tsx
@@ -9,60 +9,63 @@ import {
     IconCategory2,
 } from "@tabler/icons-react";
 
+import Overview from "./overview";
+import Artists from "./artists";
+
 export default function HeaderTabs(): JSX.Element {
     return (
-            <Tabs color="green" defaultValue="overview" autoContrast mt={50}>
-                <Tabs.List grow>
-                    <Tabs.Tab
-                        value="overview"
-                        leftSection={<IconPlaylist size={20} />}
-                    >
-                        Overview
-                    </Tabs.Tab>
+        <Tabs color="green" defaultValue="overview" autoContrast mt={50}>
+            <Tabs.List grow>
+                <Tabs.Tab
+                    value="overview"
+                    leftSection={<IconPlaylist size={20} />}
+                >
+                    Overview
+                </Tabs.Tab>
 
-                    <Tabs.Tab
-                        value="songs"
-                        leftSection={<IconMusicHeart size={20} />}
-                    >
-                        Songs
-                    </Tabs.Tab>
-                    <Tabs.Tab
-                        value="artists"
-                        leftSection={<IconHeartFilled size={20} />}
-                    >
-                        Artists
-                    </Tabs.Tab>
-                    <Tabs.Tab
-                        value="albums"
-                        leftSection={<IconAlbum size={20} />}
-                    >
-                        Albums
-                    </Tabs.Tab>
-                    <Tabs.Tab
-                        value="genres"
-                        leftSection={<IconCategory2 size={20} />}
-                    >
-                        Genre
-                    </Tabs.Tab>
-                </Tabs.List>
+                <Tabs.Tab
+                    value="songs"
+                    leftSection={<IconMusicHeart size={20} />}
+                >
+                    Songs
+                </Tabs.Tab>
+                <Tabs.Tab
+                    value="artists"
+                    leftSection={<IconHeartFilled size={20} />}
+                >
+                    Artists
+                </Tabs.Tab>
+                <Tabs.Tab
+                    value="albums"
+                    leftSection={<IconAlbum size={20} />}
+                >
+                    Albums
+                </Tabs.Tab>
+                <Tabs.Tab
+                    value="genres"
+                    leftSection={<IconCategory2 size={20} />}
+                >
+                    Genre
+                </Tabs.Tab>
+            </Tabs.List>
 
-                <Tabs.Panel value="overview">
-                    <Text size="md">Overview</Text>
-                </Tabs.Panel>
+            <Tabs.Panel value="overview">
+                <Overview />
+            </Tabs.Panel>
 
-                <Tabs.Panel value="songs">
-                    <Text size="md">Top songs</Text>
-                </Tabs.Panel>
+            <Tabs.Panel value="songs">
+                <Text size="md">Top songs</Text>
+            </Tabs.Panel>
 
-                <Tabs.Panel value="artists">
-                    <Text size="md">Top artists</Text>
-                </Tabs.Panel>
-                <Tabs.Panel value="albums">
-                    <Text size="md">Top albums</Text>
-                </Tabs.Panel>
-                <Tabs.Panel value="genres">
-                    <Text size="md">Top genres</Text>
-                </Tabs.Panel>
-            </Tabs>
+            <Tabs.Panel value="artists">
+                <Artists />
+            </Tabs.Panel>
+            <Tabs.Panel value="albums">
+                <Text size="md">Top albums</Text>
+            </Tabs.Panel>
+            <Tabs.Panel value="genres">
+                <Text size="md">Top genres</Text>
+            </Tabs.Panel>
+        </Tabs>
     );
 }
