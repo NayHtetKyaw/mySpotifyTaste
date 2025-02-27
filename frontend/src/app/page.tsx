b1357995c037dccce4eb385b36e9eb6fe07b367e
NayHtetKyaw <nayhtetkyaw.dev@gmail.com>
2025-02-27 20:33:36 +0700

    landing page

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,65 @@
+import { Title, Text, Container, Button, Flex, Box } from "@mantine/core";
+import Image from "next/image";
+import { IconCircleCheck } from "@tabler/icons-react";
+
+const features = [
+  "Visualize your Spotify listening history & stats",
+  "View your top tracks, artists, and genres",
+  "Discover new music based on your listening habits",
+  "Compare your listening habits with friends",
+  "Share your listening history with others",
+];
+
+export default function HomePage() {
+  return (
+    <Container p="lg">
+      <Flex justify="center" direction="column">
+        <Box className="flex flex-col items-center text-center" m="lg">
+          <Title size={50}>Welcome to MySpotifyTaste</Title>
+          <Title order={3} c="green" my="md">
+            Visualize your Spotify listening history & stats
+          </Title>
+        </Box>
+
+        <Flex wrap="wrap" justify="space-evenly" direction="row" align="center">
+          <Box>
+            <Image
+              src="/assets/images/myspotifytaste.png"
+              width={400}
+              height={400}
+              alt="Spotify Logo"
+              className="rounded-full"
+            />
+          </Box>
+          <Box mt="xl">
+            <Title order={2} size={30}>
+              Features
+            </Title>
+            {features.map((feature) => (
+              <Flex key={feature} align="start" gap="xs" my="sm">
+                <IconCircleCheck
+                  size={25}
+                  className="text-green-400 shrink-0"
+                />
+                <Text className="text-start self-center" size="xl">
+                  {feature}
+                </Text>
+              </Flex>
+            ))}
+
+            <Button
+              my="md"
+              radius="sm"
+              variant="filled"
+              color="green"
+              fullWidth
+              size="lg"
+            >
+              Get Started
+            </Button>
+          </Box>
+        </Flex>
+      </Flex>
+    </Container>
+  );
+}
