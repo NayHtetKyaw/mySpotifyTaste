0c655c3e492170d698345ef47a8bd5962d73a1f2
pantorn <longpantorn@gmail.com>
2025-04-27 21:31:01 +0700

    add light

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,29 +1,44 @@
 import { BlurFade } from "@/components/ui/blur-fade";
 import { Disc3 } from "lucide-react";
-import { Flex, Text } from "@radix-ui/themes";
+import { Flex, Heading, Text } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import { InteractiveHoverButton } from "@/components/magicui/interactive-hover-button";
 
 export default function Home() {
 	return (
-		<div className="flex items-center justify-center flex-col w-screen h-screen ">
-			<section id="header">
-				<BlurFade delay={0.25} inView>
-					<h2 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none mb-2 text-green-400">
-						MySpotifyTaste{" "}
-						<Disc3 className="inline duration-[5s] animate-spin " size={48} />
-					</h2>
-				</BlurFade>
-				<BlurFade delay={0.25 * 2} inView>
-					<span className="text-xl text-pretty tracking-tighter sm:text-3xl xl:text-4xl/none">
-						Keep track of your <span className="text-green-200">taste</span> on
-						<span className="text-green-200"> spotify</span>
-					</span>
-				</BlurFade>
-			</section>
+		<div className="relative flex items-center justify-center flex-col w-screen h-screen ">
+			{/* <div className="absolute top-0 z-[0] h-screen w-screen bg-green-950/10 dark:bg-green-950/10 bg-[radial-gradient(ellipse_20%_80%_at_50%_-20%,rgba(120,119,198,0.15),rgba(255,255,255,0))] dark:bg-[radial-gradient(ellipse_20%_80%_at_50%_-20%,rgba(119, 198, 120, 0.3),rgba(255,255,255,0))]" /> */}
+			{/* <div className="absolute top-0 z-[0] h-screen w-screen bg-black dark:bg-black bg-[radial-gradient(ellipse_20%_80%_at_50%_-20%,rgba(120,119,198,0.15),rgba(255,255,255,0))] dark:bg-[radial-gradient(ellipse_20%_80%_at_50%_-20%,rgba(119, 198, 120, 0.3),rgba(255,255,255,0))]" /> */}
+
+
+			{/* bright3 */}
+			<div
+				className="absolute top-0 z-[0] h-screen w-screen bg-black dark:bg-black
+  bg-[radial-gradient(ellipse_40%_80%_at_50%_-20%,rgba(119,198,120,0.6),rgba(255,255,255,0))]
+  dark:bg-[radial-gradient(ellipse_40%_80%_at_50%_-20%,rgba(119,198,120,0.6),rgba(255,255,255,0))]
+"
+			/>
+			<BlurFade delay={0.25} inView>
+				<Heading
+					size={"9"}
+					className="font-bold tracking-tighter mb-2 text-green-400"
+				>
+					MySpotifyTaste{" "}
+					<Disc3 className="inline duration-[5s] animate-spin " size={48} />
+				</Heading>
+			</BlurFade>
+			<BlurFade delay={0.25 * 2} inView>
+				<Heading
+					size={"8"}
+					className="text-pretty tracking-tighter sm:text-3xl xl:text-4xl/none"
+				>
+					Keep track of your <span className="text-green-200">taste</span> on
+					<span className="text-green-200"> spotify</span>
+				</Heading>
+			</BlurFade>
 			<BlurFade className="max-w-3xl mt-4" delay={0.25 * 3.5} inView>
 				<Flex align={"center"}>
-					<Text size="4" align="center" wrap={"pretty"}>
+					<Text size="4" weight={"regular"} align="center" wrap={"pretty"}>
 						An application that keeps track of your Spotify listening habits,
 						analyzes your music taste over time, and gives you insights into
 						your top songs, artists, and genres. Discover your trends, compare
@@ -31,8 +46,8 @@ export default function Home() {
 					</Text>
 				</Flex>
 			</BlurFade>
-			<BlurFade className="max-w-3xl mt-4" delay={0.25 * 4.5} inView>
-				<InteractiveHoverButton>Hover Me</InteractiveHoverButton>
+			<BlurFade className="max-w-3xl mt-4" delay={0.25 * 4} inView>
+				<InteractiveHoverButton>Get Started</InteractiveHoverButton>
 			</BlurFade>
 		</div>
 	);
