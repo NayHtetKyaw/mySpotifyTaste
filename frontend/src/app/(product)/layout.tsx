343752780864ff0ae2e72b1f7408bb6319ae9ffd
pantorn <longpantorn@gmail.com>
2025-05-06 19:35:09 +0700

    add tabnav

--- frontend/src/app/(product)/layout.tsx
+++ frontend/src/app/(product)/layout.tsx
@@ -1,13 +1,18 @@
-import {TabNav} from "@radix-ui/themes"
-
+import { TabNav } from "@radix-ui/themes";
 
 export default function RootLayout({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
 		<>
-			<div>
-				hello
+			<div className="">
+				<TabNav.Root className="">
+					<TabNav.Link href="#" active>
+						account
+					</TabNav.Link>
+					<TabNav.Link href="#">Documents</TabNav.Link>
+					<TabNav.Link href="#">Settings</TabNav.Link>
+				</TabNav.Root>{" "}
 				{children}
 			</div>
 		</>
