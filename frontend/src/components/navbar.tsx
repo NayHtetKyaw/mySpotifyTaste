5f2ebac796a1561fe3727c6e091c6d8cd907828f
pantorn <longpantorn@gmail.com>
2025-05-10 13:13:30 +0700

    link navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,14 +1,15 @@
 import { Flex, TabNav } from "@radix-ui/themes";
 
 export default function Navbar() {
-    return (
-        <Flex justify="center" width="full">
-            <TabNav.Root className="font-bold text-white">
-                <TabNav.Link href="#dashboard">Dashboard</TabNav.Link>
-                <TabNav.Link href="#artist">Artists</TabNav.Link>
-                <TabNav.Link href="#song">Songs</TabNav.Link>
-                <TabNav.Link href="/contact-us">Contact us</TabNav.Link>
-            </TabNav.Root>
-        </Flex>
-    );
+	return (
+		<Flex justify="center" width="full">
+			<TabNav.Root className="font-bold text-white">
+				<TabNav.Link href="/dashboard">Dashboard</TabNav.Link>
+				<TabNav.Link href="/artist">Artists</TabNav.Link>
+				<TabNav.Link href="/song">Songs</TabNav.Link>
+				<TabNav.Link href="/contact-us">Contact us</TabNav.Link>
+			</TabNav.Root>
+		</Flex>
+	);
 }
+
